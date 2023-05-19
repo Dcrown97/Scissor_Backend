d7b255ef01ccdc73303020148605d9887f5fc5fe
Aisimiyu Oluwadarasimi <aisimiyuoluwadara@gmail.com>
2023-05-19 15:00:35 +0100

    db settigns updated

--- database/db.js
+++ database/db.js
@@ -4,16 +4,26 @@ require('dotenv').config();
 const MONGO_DB_CONNECTION_URL = process.env.MONGO_DB_CONNECTION_URL
 
 // create database connectioon
-function connectToDb() {
-    mongoose.connect(MONGO_DB_CONNECTION_URL || 'http://localhost:3000')
+const connectToDb = async () => {
+    try {
+        await mongoose.connect(MONGO_DB_CONNECTION_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Database Connected');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+    // mongoose.connect(MONGO_DB_CONNECTION_URL || 'http://localhost:3000')
 
-    mongoose.connection.on("connected", () => {
-        console.log("connected to MongoDb Successfully");
-    })
+    // mongoose.connection.on("connected", () => {
+    //     console.log("connected to MongoDb Successfully");
+    // })
 
-    mongoose.connection.on("error", (err) => {
-        console.log(err, "An error occured while connecting to the MongoDb")
-    })
+    // mongoose.connection.on("error", (err) => {
+    //     console.log(err, "An error occured while connecting to the MongoDb")
+    // })
 }
 
 module.exports = { connectToDb };
