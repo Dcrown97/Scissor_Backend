932724d4918b1755c64c6cee6217178eb85f40ca
Aisimiyu Oluwadarasimi <aisimiyuoluwadara@gmail.com>
2023-06-20 17:55:02 +0100

    .

--- routes/urlRoute.js
+++ routes/urlRoute.js
@@ -10,7 +10,7 @@ urlRouter.post('/api/short', limiter, authorizationToken, userToken, urlControll
 urlRouter.post('/api/custom', limiter, authorizationToken, userToken, urlController.custom);
 urlRouter.get('/:urlId', limiter, urlController.redirect);
 urlRouter.get('/api/analytics', limiter, authorizationToken, userToken, urlController.analytics);
-urlRouter.get('/api/visit/:id', authorizationToken, userToken, urlController.visit);
+urlRouter.get('/api/visit/:id', limiter, authorizationToken, userToken, urlController.visit);
 urlRouter.get('/api/user/:id', authorizationToken, userToken, urlController.users);
 
 module.exports = urlRouter;
